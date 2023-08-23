18616ec26e01cbd4de9b3b6b40d2063e928b4fea
THISISAGOODNAME <yangyj19931231@gmail.com>
2023-08-23 19:05:57 +0800

    add raii triangle

---
+++ raii/ch0_devenv/ch0_devenv.cpp
@@ -0,0 +1,43 @@
+#define GLFW_INCLUDE_NONE
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <vulkan/vulkan_raii.hpp>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan windows", nullptr, nullptr);
+
+    vk::raii::Context context;
+
+    std::vector<vk::ExtensionProperties> extensions = context.enumerateInstanceExtensionProperties();
+
+    std::cout << extensions.size() << " extensions supported" << std::endl;
+    for (auto& extension : extensions) {
+        std::cout << "    " << extension.extensionName << std::endl;
+    }
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
+}
